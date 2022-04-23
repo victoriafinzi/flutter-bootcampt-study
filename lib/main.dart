be49bb808ab443f2ea61ce095abf633e21ed41a5
victoriafinzi <victoria.finzi15@gmail.com>
2022-04-23 10:11:39 -0300

    the magic ball app is donegit add .

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Magic Ball')),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey[100],
+        body: MagicBall(),
+      ),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
+class MagicBall extends StatefulWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<MagicBall> createState() => _MagicBallState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MagicBallState extends State<MagicBall> {
+  var clickBall = 1;
+
+  void changeMagicBall() {
+    setState(() {
+      clickBall = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  changeMagicBall();
+                });
+              },
+              child: Image.asset('images/ball$clickBall.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
